0ab6d30b5398aa1ee24428ddec2c46f8cefd8cb2
Bradley Bossard <bradleybossard@gmail.com>
2019-08-07 11:37:25 -0700

    Update code to loop through an manually

--- day1/canvas2d.js
+++ day1/canvas2d.js
@@ -2,3 +2,16 @@ const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
 ctx.fillRect(0, 0, 100, 50);
+function fillRect(top, left, width, height) {
+  const pixelStore = new Uint8ClampedArray(canvas.width * canvas.height * 4);
+  for (let i = 0; i < pixelStore.length; i += 4) {
+    pixelStore[i] = 0; // r
+    pixelStore[i + 1] = 0; // g
+    pixelStore[i + 2] = 200; // b
+    pixelStore[i + 3] = 255; // alpha
+  }
+  const imageData = new ImageData(pixelStore, canvas.width, canvas.height);
+  ctx.putImageData(imageData, 0, 0);
+}
+
+fillRect();
