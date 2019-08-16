82aac2b7bac495358d3f51d831de1915224954a9
Bradley Bossard <bradleybossard@gmail.com>
2019-08-16 20:31:14 +0000

    Initial JS to compile vertex shader

---
+++ day2/canvas2d.js
@@ -0,0 +1,19 @@
+const canvas = document.querySelector('canvas');
+const gl = canvas.getContext('webgl');
+
+const program = gl.createProgram();
+
+const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+
+const vShaderSource =
+`
+void main() {
+  gl_Position = vec4(0, 0, 0, 1);
+ }
+ `;
+
+gl.shaderSource(vertexShader, vShaderSource);
+gl.compileShader(vertexShader);
+
+console.log(gl.getShaderInfoLog(vertexShader));
