05c7bc2e3399c7d40497ba4e456448a3c3f8d3fb
Bradley Bossard <bradleybossard@gmail.com>
2019-08-18 16:18:46 +0000

    Delete dead code

--- day3/canvas2d.js
+++ day3/canvas2d.js
@@ -15,10 +15,8 @@ uniform float width;
 uniform vec2 resolution;
 
 void main() {
-  // float x = position.x / width * 2.0 - 1.0;
   vec2 transformedPosition = position / resolution * 2.0 - 1.0;
   gl_PointSize = 2.0;
-  // gl_Position = vec4(x, cos(x * M_PI), 0, 1);
   gl_Position = vec4(transformedPosition, 0, 1);
  }
 `;
@@ -29,7 +27,6 @@ precision mediump float;
 uniform vec4 color;
 
 void main() {
-  //gl_FragColor = vec4(1, 0, 0, 1);
   gl_FragColor = color / 255.0;
 }
 `;
@@ -57,42 +54,24 @@ gl.useProgram(program);
 
 const positionPointer = gl.getAttribLocation(program, 'position');
 
-// const widthUniformLocation = gl.getUniformLocation(program, 'width');
 const resolutionUniformLocation = gl.getUniformLocation(program, 'resolution');
-// gl.uniform1f(widthUniformLocation, canvas.width);
 gl.uniform2fv(resolutionUniformLocation, [canvas.width, canvas.height]);
 
 const colorUniformLocation = gl.getUniformLocation(program, 'color');
 gl.uniform4fv(colorUniformLocation, [200, 0, 0, 255]);
 
-//const points = [];
 const lines = [];
-//let prevLineY = 0;
 const triangles = [
   0, 0, // v1 (x, y)
   canvas.width / 2, canvas.height, // v2 (x, y)
   canvas.width, 0, // v3 (x, y)
 ];
 
-/*
-for (let i = 0; i < canvas.width - 5; i+=5) {
-  // points.push(i, i);
-  lines.push(i, prevLineY);
-  const y =  Math.random() * canvas.height;
-  lines.push(i + 5, y);
-
-  prevLineY = y;
-}
-*/
-// const positionData = new Float32Array(points);
-// const positionData = new Float32Array(lines);
 const positionData = new Float32Array(triangles);
 
 const positionBuffer = gl.createBuffer(gl.ARRAY_BUFFER);
 gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 gl.bufferData(gl.ARRAY_BUFFER, positionData, gl.STATIC_DRAW);
-// Change line width
-//gl.lineWidth(10);
 
 const attributeSize = 2;
 const type = gl.FLOAT;
@@ -103,6 +82,4 @@ const offset = 0;
 gl.vertexAttribPointer(positionPointer, attributeSize, type, normalized, stride, offset);
 gl.enableVertexAttribArray(positionPointer);
 
-//gl.drawArrays(gl.POINTS, 0, positionData.length / 2);
-//gl.drawArrays(gl.LINES, 0, positionData.length / 2);
 gl.drawArrays(gl.TRIANGLES, 0, positionData.length / 2);
